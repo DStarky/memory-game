be3991519b8aa391f4f9ccb5d64ba8d9afc3f86e
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-04-11 10:33:53 +0500

    added colors for buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,18 @@ interface ButtonProps {
 }
 
 const Button = ({ color }: ButtonProps) => {
+  const colorStyles = {
+    violet: 'bg-violet-500 border-b-violet-700',
+    red: 'bg-red-500 border-b-red-700',
+    blue: 'bg-blue-500 border-b-blue-700',
+    green: 'bg-green-500 border-b-green-700',
+  };
+
+  const buttonStyle = `group flex h-16 w-48 items-center hover:opacity-95 justify-center ring-none rounded-lg shadow-lg font-semibold py-2 px-4 ${colorStyles[color]}`;
+
   return (
     <>
-      <button
-        className={`group flex h-16 w-48 items-center hover:opacity-95 justify-center ring-none rounded-lg shadow-lg font-semibold py-2 px-4 bg-${color}-500 border-b-${color}-700`}
-      />
+      <button className={buttonStyle} />
     </>
   );
 };
