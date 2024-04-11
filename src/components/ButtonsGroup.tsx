230f7fcf8ef66fe4a14ee2fe2b4cb893201204f1
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-04-11 15:43:48 +0500

    added title

--- src/components/ButtonsGroup.tsx
+++ src/components/ButtonsGroup.tsx
@@ -59,33 +59,42 @@ const ButtonsGroup = () => {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -50 }}
       transition={{ duration: 1, delay: 2 }}
-      className="grid grid-cols-[1fr_64px_1fr]  max-sm:grid-cols-[1fr_32px_1fr] gap-8 max-md:gap-6 max-sm:gap-4 justify-items-center"
     >
-      <Button
-        isActive={buttonStates[1]}
-        color={ButtonColors.BLUE}
-        direction="vertical"
-        className="col-start-2"
-      />
-      <Button
-        isActive={buttonStates[2]}
-        color={ButtonColors.RED}
-        direction="horizontal"
-        className="col-start-1"
-      />
-      <div className="center col-start-2 w-2"></div>
-      <Button
-        isActive={buttonStates[3]}
-        color={ButtonColors.GREEN}
-        direction="horizontal"
-        className="col-start-3"
-      />
-      <Button
-        isActive={buttonStates[4]}
-        color={ButtonColors.VIOLET}
-        direction="vertical"
-        className="col-start-2"
-      />
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 3 }}
+        className="text-3xl absolute top-12 left-1/2 -translate-x-1/2 font-title"
+      >
+        Уровень: <strong>{seq.length}</strong>
+      </motion.h1>
+      <div className="grid grid-cols-[1fr_64px_1fr]  max-sm:grid-cols-[1fr_32px_1fr] gap-8 max-md:gap-6 max-sm:gap-4 justify-items-center">
+        <Button
+          isActive={buttonStates[1]}
+          color={ButtonColors.BLUE}
+          direction="vertical"
+          className="col-start-2"
+        />
+        <Button
+          isActive={buttonStates[2]}
+          color={ButtonColors.RED}
+          direction="horizontal"
+          className="col-start-1"
+        />
+        <div className="center col-start-2 w-2"></div>
+        <Button
+          isActive={buttonStates[3]}
+          color={ButtonColors.GREEN}
+          direction="horizontal"
+          className="col-start-3"
+        />
+        <Button
+          isActive={buttonStates[4]}
+          color={ButtonColors.VIOLET}
+          direction="vertical"
+          className="col-start-2"
+        />
+      </div>
     </motion.div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'primary': ["Bungee Spice"]
+        'primary': ["Bungee Spice"],
+        'title': ["Poppins"]
       }
     },
   },
