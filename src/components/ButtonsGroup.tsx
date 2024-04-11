e09f67a71bcfc8534ec36b4da9d37c1c281e9b33
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-04-11 12:52:36 +0500

    added order

--- src/components/ButtonsGroup.tsx
+++ src/components/ButtonsGroup.tsx
@@ -12,22 +12,26 @@ const ButtonsGroup = () => {
       className="grid grid-cols-[1fr_64px_1fr]  max-sm:grid-cols-[1fr_32px_1fr] gap-8 max-md:gap-6 max-sm:gap-4 justify-items-center"
     >
       <Button
+        order={1}
         color="blue"
         direction="vertical"
         className="col-start-2"
       />
       <Button
+        order={2}
         color="green"
         direction="horizontal"
         className="col-start-1"
       />
       <div className="center col-start-2 w-2"></div>
       <Button
+        order={3}
         color="red"
         direction="horizontal"
         className="col-start-3"
       />
       <Button
+        order={4}
         color="violet"
         direction="vertical"
         className="col-start-2"
